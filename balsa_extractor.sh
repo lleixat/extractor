a3783185b9e4d129d6127f9ee1d22a6483e1dbab
lleixat <lleixat@gmail.com>
2012-04-30 16:21:22 +0200

    added -help option

--- balsa_extractor.sh
+++ balsa_extractor.sh
@@ -11,30 +11,63 @@ function_construct ()
 	echo -e "\n"
 }
 
+show_help ()
+{
+saveIFS=$IFS
+IFS='\n' read -r -d '' HELP << 'EOF'
+Usage:  extractor [option] [file] <-wip
+        extractor --class
+
+Options :
+ -sc,  --style-class     Search for html "class" attribute.
+  -c,  --class           Search for php classes.
+  -f,  --function        Search for php method/function name.
+  -p,  --parameters      Search for php method/functions parameters.
+ -fp,  --function-param  Search for php method/functions and parameters.
+-com,  --comments        Search for php comments.
+  -h,  --help            This help.
+
+EOF
+
+echo "$HELP"
+
+IFS=$saveIFS
+
+}
+
+
 case "$1" in
-	-sc|style-class)
+	-sc|--style-class)
 		COM="Style Class"
 		PATTERN='class="\K.*?(?=")'
 		;;
-	-c|class)
+	-c|--class)
 		COM="Classes"
 		PATTERN='class \K.*?(?=\n)'
 		;;
-	-f|function)
+	-f|--function)
 		COM="function"
 		PATTERN='function \K.*?(?=\()'
 		;;
-	-p|funcparam)
+	-p|--parameters)
 		COM="Functions parameters"
 		PATTERN='function .*\(\K.*?(?=\))'
 		;;
-	-fp|funcandparam)
+	-fp|--function-param)
 		COM="Functions with parameters"
 		PATTERN='function .*\(\K.*?(?=\n)'
 		;;
-	-com|comments)
+	-com|--comments)
 		COM="Functions with parameters"
 		PATTERN='\/\*\K.*?(?=(\*\/\n))'
+		;;
+	-h|--help)
+		show_help
+		exit 0
+		;;
+	*)
+		show_help
+		exit 0
 		;;
 esac
 
